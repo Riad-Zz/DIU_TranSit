a66ab5bffe5f0b5e2afa62f5da5c8e8980d1a39a
Md.Riadul Islam Riad <kazuha3242@gmail.com>
2026-04-10 21:59:07 +0600

    Login Page Added (UI Only)

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,13 +1,22 @@
 import { createBrowserRouter } from "react-router";
 import Root from "../Layout/Root/Root";
 import Home from "../Pages/Home/Home/Home";
+import Login from "../Pages/Authentication/Login";
+import AuthLayout from "../Layout/AuthLayout/AuthLayout";
 
 export const router = createBrowserRouter([
     {
         path : '/',
         Component : Root,
         children : [
-            {index : true , Component : Home}
+            {index : true , Component : Home},
+        ]
+    },
+    {
+        path : '/',
+        Component : AuthLayout ,
+        children : [
+            {path:'/login' , Component : Login}
         ]
     }
 ])
